5d0a43ae17127034dd12c0482c705a65bf8485c2
ArtienVoskanian <arvoskani@gmail.com>
2023-11-02 15:03:58 -0700

    Adding file for main code under main.py

---
+++ PyBank/main.py/PyBank Challenge.py
@@ -0,0 +1,27 @@
+# PyBank Challenge
+
+#Let's import the necessary modules to read and utilize csv files
+
+import os
+import csv
+
+#Set up a filepath that references the budget_data.csv
+
+print("hello world")
+#Open and read budget_data.csv
+
+
+#Write a script that analyzes/calculates the following:  
+#Total number of months included in the dataset
+    
+
+#The net total amount of "Profit/Losses" over the entire period
+
+#The changes in "Profit/Losses" over the entire period,
+#and then averages those changes
+
+#The greatest increase in profits (date and amount) over entire period
+
+#The greatest decrease in profits (date and amount) over the entire period
+
+#Analysis must be printed to terminal AND exported to a txt file
